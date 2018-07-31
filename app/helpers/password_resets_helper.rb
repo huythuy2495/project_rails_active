d2e0ad46f1dd98f71048db5171273f9bbca3633c
huy thuy <jbhuythuy@gmail.com>
2018-07-31 20:25:10 +0700

    reset password user

---
+++ app/helpers/password_resets_helper.rb
@@ -0,0 +1,14 @@
+module PasswordResetsHelper
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = t(".info")
+      redirect_to root_url
+    else
+      flash.now[:danger] = t(".danger")
+      render :new
+    end
+  end
+end
