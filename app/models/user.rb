e754027d4009f23496cdcc6c99b955bc2478b616
huy thuy <jbhuythuy@gmail.com>
2018-08-01 16:13:05 +0700

    User microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -60,7 +61,11 @@ class User < ApplicationRecord
   end
 
   def password_reset_expired?
-    reset_sent_at < 2.hours.ago
+    reset_sent_at < Settings.time_expired.hours.ago
+  end
+
+  def feed
+    microposts
   end
 
 
